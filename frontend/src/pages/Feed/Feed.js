21b28a5df99a6a6b63ee37c31bf6bbb542ff820c
Manav Vanecha <manavpatel701@gmail.com>
2024-12-10 03:07:58 -0500

    deploy poc

--- frontend/src/pages/Feed/Feed.js
+++ frontend/src/pages/Feed/Feed.js
@@ -26,7 +26,7 @@ const Feed = () => {
 	useEffect(() => {
 		// fetch all posts from database
 		axios
-			.get(`http://localhost:5001/post/view` , 
+			.get(`/post/view` , 
 				{
 					headers : {
 						'Authorization' : `Bearer ${token}`
@@ -37,14 +37,14 @@ const Feed = () => {
 			});
 		// detch all live updates from database
 		axios
-			.get(`http://localhost:5001/liveupdate/view`)
+			.get(`/liveupdate/view`)
 			.then(function (response) {
 				dispatch(createLiveUpdate(response.data));
 			})
 			.catch((err) => {
 				console.log(err);
 			});
-		axios.get('http://localhost:5001/trip' , {
+		axios.get('/trip' , {
 			headers : {
 				'Authorization' : `Bearer ${token}`
 			}
